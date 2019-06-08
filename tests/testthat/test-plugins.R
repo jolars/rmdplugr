c805f8df889ac4613498b4e16f4cca6581477512
Johan Larsson <mail@larssonjohan.com>
2019-06-08 21:30:31 +0200

    Adopt tests if pandoc is not available

--- tests/testthat/test-plugins.R
+++ tests/testthat/test-plugins.R
@@ -1,7 +1,12 @@
 test_that("plugins are correctly applied", {
-  format <- pdf_article()
-  expect_s3_class(format, "rmarkdown_output_format")
+  if (rmarkdown::pandoc_available()) {
+    format <- pdf_article()
+    expect_s3_class(format, "rmarkdown_output_format")
 
-  format <- pdf_presentation()
-  expect_s3_class(format, "rmarkdown_output_format")
+    format <- pdf_presentation()
+    expect_s3_class(format, "rmarkdown_output_format")
+  } else {
+    expect_error(pdf_article())
+    expect_error(pdf_presentation())
+  }
 })
